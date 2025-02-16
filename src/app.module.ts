ca6f740e7fe01b4337780d8f8983614a1ae696fe
duoduoxu <duoduoxu@tencent.com>
2025-02-16 20:47:34 +0800

    feat: module init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ArticleModule } from './article/article.module';
+import { RedisModule } from './redis/redis.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ArticleModule, RedisModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
